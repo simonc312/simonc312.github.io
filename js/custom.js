f3d9c716f4ae78765f2be825775d8df428136519
simonc312 <simon.c312@berkeley.edu>
2015-03-29 21:34:32 -0700

    add swipe-rotate animation for portfolio-item

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,51 @@
-function toggleNav() {
-    if ($('#site-wrapper').hasClass('show-nav')) {
-        // Do things on Nav Close
-        $('#site-wrapper').removeClass('show-nav');
-    } else {
-        // Do things on Nav Open
-        $('#site-wrapper').addClass('show-nav');
-    }
-
-    //$('#site-wrapper').toggleClass('show-nav');
-}
-
+$(document).ready(function(){
+    $('.js .grid').addClass('loaded');
+});
 
 $(window).load(function() {
+    $('.js .grid').load(function(){ $(this).addClass('loaded')});
 
-$('.sidebar-nav li').hover(
-	function(){  $('#site-wrapper').addClass('show-nav'); },
-	function(){ }
-	);
+    $('.sidebar-nav li').hover(
+    	function(){  $('#site-wrapper').addClass('show-nav'); },
+    	function(){ }
+    	);
 
-$('#site-submenu').hover(
-            function() {},
-            function() {$('#site-wrapper').removeClass('show-nav');}
-            );
+    $('#site-submenu').hover(
+                function() {},
+                function() {$('#site-wrapper').removeClass('show-nav');}
+                );
 
-}
-);
+    var addCurtain = function(image,anchor) {
+        if( !image ) return;
+        var curtain = document.createElement( 'div' );
+        curtain.className = 'curtain';
+        var rgb = new ColorFinder( function favorHue(r,g,b) {
+            // exclude white
+            //if (r>245 && g>245 && b>245) return 0;
+            return (Math.abs(r-g)*Math.abs(r-g) + Math.abs(r-b)*Math.abs(r-b) + Math.abs(g-b)*Math.abs(g-b))/65535*50+1;
+        } ).getMostProminentColor( image );
+        if( rgb.r && rgb.g && rgb.b ) {
+            curtain.style.background = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+        }
+        anchor.appendChild( curtain );
+         $(anchor).find('.curtain').one('webkitAnimationEnd oanimationend oAnimationend msAnimationEnd animationend',function(e){
+            $(anchor).removeClass('animate');
+            $(anchor).addClass('shown');
+        });
+    };
+
+    //iterate over items to be displayed and add curtains at random range 0-500ms
+    $('.js .grid .portfolio-item').each(function(index,item){
+        
+        if($(item).hasClass('shown'))
+            return;
+
+        var random_delay = Math.random()*500;
+        window.setTimeout(function(){
+            addCurtain($(item).find('img')[0],item);
+        },random_delay);
+       
+        $(item).addClass('animate');
+    });
+
+});
